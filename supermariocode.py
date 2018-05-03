f56b03b3fc1d35f8e07b1a6721db7ce7c7607c0a
hannahhnguyen <38928433+hannahhnguyen@users.noreply.github.com>
2018-05-02 19:32:29 -0500

    Create supermariocode.py

---
+++ supermariocode.py
@@ -0,0 +1,377 @@
+
+
+import sys
+import os
+import time
+
+
+#Set up player with a class
+class player:
+    def __init__(self):
+        self.name = ""
+        self.character = ""
+        self.hp = 0
+        self.coins = 0
+        self.location = "Start Point"
+        self.gameOver = False
+
+playerOne = player()
+
+#title screen using a function and using if statements
+#ENTER CODE HERE
+
+def game_start():
+    print("Welcome to Super Mario Code! ")#add more to it later
+
+
+
+
+
+#creating the map - grid system
+
+#  1   2   3   4
+#------------------
+#|   |   |   |   |  a
+#------------------
+#|   |   |   |   |  b
+#------------------
+#|   |   |   |   |  c
+#------------------
+#|   |   |   |   |  d
+#------------------
+
+#navigating the map
+nameOfZone = ""
+description = "description"
+look = "examine"
+solved = False
+moveUp = "up"
+moveDown = "down"
+moveLeft = "left"
+moveRight = "right"
+
+#dictionary of all the solved places
+solvedPlaces = {"a1": False, "a2": False, "a3": False, "a4": False,
+                "b1": False, "b2": False, "b3": False, "b4": False,
+                "c1": False, "c2": False, "c3": False, "c4": False,
+                "d1": False, "d2": False, "d3": False, "d4": False,
+                "e1": False, "e2": False, "e3": False, "e4": False,}
+#setting up the map
+gameMap = {
+    "a1": {
+        nameOfZone: "",
+        description: "Home",
+        look: "This is your home.",
+        solved: False,
+        moveUp: "",
+        moveDown: "b1",
+        moveLeft: "",
+        moveRight: "a2"
+    },
+    "a2": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "",
+        moveDown: "b2",
+        moveLeft: "a1",
+        moveRight: "a3"
+    },
+    "a3": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "",
+        moveDown: "b3",
+        moveLeft: "b2",
+        moveRight: "b4"
+    },
+    "a4": {
+        nameOfZone: "",
+        description: "Home",
+        look: "This is your home.",
+        solved: False,
+        moveUp: "",
+        moveDown: "b4",
+        moveLeft: "b3",
+        moveRight: ""
+    },
+    "b1": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "a1",
+        moveDown: "c1",
+        moveLeft: "",
+        moveRight: "b2"
+    },
+    "b2": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "a2",
+        moveDown: "c2",
+        moveLeft: "b1",
+        moveRight: "b3"
+    },
+    "b3": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "a3",
+        moveDown: "c3",
+        moveLeft: "b2",
+        moveRight: "b4"
+    },
+    "b4": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "a4",
+        moveDown: "c4",
+        moveLeft: "b3",
+        moveRight: ""
+    },
+    "c1": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "b1",
+        moveDown: "d1",
+        moveLeft: "",
+        moveRight: "c2"
+    },
+    "c2": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "b2",
+        moveDown: "d2",
+        moveLeft: "c1",
+        moveRight: "c3"
+    },
+    "c3": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "b3",
+        moveDown: "d3",
+        moveLeft: "c2",
+        moveRight: "c4"
+    },
+    "c4": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "b4",
+        moveDown: "d4",
+        moveLeft: "c3",
+        moveRight: ""
+    },
+    "d1": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "c1",
+        moveDown: "",
+        moveLeft: "",
+        moveRight: "d2"
+    },
+    "d2": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "c2",
+        moveDown: "",
+        moveLeft: "d1",
+        moveRight: "d3"
+    },
+    "d3": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "c3",
+        moveDown: "",
+        moveLeft: "d2",
+        moveRight: "d4"
+    },
+    "d4": {
+        nameOfZone: "",
+        description: "",
+        look: "",
+        solved: False,
+        moveUp: "c4",
+        moveDown: "",
+        moveLeft: "d3",
+        moveRight: ""
+    },
+
+}
+
+
+#this prints where the player is at in the map
+def print_location():
+    print(playerOne.location.upper())
+    print( gameMap[playerOne.position][description])
+
+
+#actions that the player can take PROMPT
+def prompt():
+    print( "You can move, go, travel, walk, quit, examine, inspect, and look.")
+    action = input("What would you like to do? >>> ")
+    acceptableActions = ["move", "go", "travel", "walk", "quit", "examine", "inspect", "look"]
+    #if they dont put in an acceptable action...
+    while action.lower() not in acceptableActions:
+        print("Undefined action. Please enter an acceptable action.")
+        action = input("What would you like to do? >>> ")
+    if action.lower() == "quit":
+        sys.exit()
+    elif action.lower() in ["move", "go", "travel", "walk"]:
+        player_move(action.lower())
+    elif action.lower() in ["examine", "inspect", "look"]:
+        player_look(action.lower())
+
+#for moving the player with answers from the player
+def player_move(playerAction):
+    print("You can move up, down, left, or right. Be sure not to move outside of the map!")
+    destination = input("Where would you like to go? >>> ")
+    if destination in ["up"]:
+        destination = gameMap[playerOne.location][moveUp]
+        movement_controller(destination)
+    elif destination in ["down"]:
+        destination = gameMap[playerOne.location][moveDown]
+        movement_controller(destination)
+    elif destination in ["left"]:
+        destination = gameMap[playerOne.location][moveLeft]
+        movement_controller(destination)
+    elif destination in ["right"]:
+        destination = gameMap[playerOne.location][moveRight]
+        movement_controller(destination)
+
+#more code to control the movement of the player
+def movement_controller():
+    destination = playerOne.location
+    print ("You have moved to the" + destination)
+    print_location()
+
+#for looking at or examining the spot... trivia questions go here
+#######LUIS STUFF HERE
+def player_look():
+    if gameMap[playerOne.location][solved] == True:
+        print("You have already solved this zone.")
+    else:
+        print("")
+
+
+#this is the actual game part
+def game():
+    while player.gameOver == False: #this keeps the game going until its over
+        prompt() #solving puzzles for game/ finishing the track since its mario cart?
+
+
+#game setup
+def game_setup():
+    os.system("clear")
+
+    #getting the players name
+    questionName = "Hello PLAYER, what is your name?"
+    #cool thing i learned online to make it print fancy like a game
+    for words in questionName:
+        sys.stdout.write(words)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    playerName = input(">>> ")
+    playerOne.name = playerName
+
+    # getting the character the player wants to use
+    characterOptions = "The characters you can play as are Mario, Luigi, Kerby, and Bowser."
+    for words in characterOptions:
+        sys.stdout.write(words)
+        sys.stdout.flush()
+        time.sleep(0.01)
+    questionCharacter = "What character would you like to use?"
+    for words in questionName:
+        sys.stdout.write(words)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    playerCharacter = input(">>> ")
+
+    #check to see if the character is valid
+    validCharacters = ["mario", "luigi", "kerby", "bowser"]
+    if playerCharacter.lower() in validCharacters:
+        playerOne.character = playerCharacter
+        print("You are now " + playerCharacter)
+    while playerCharacter.lower() not in validCharacters:
+        playerCharacter = input("Unknown character. Please enter a character in this game. >>>")
+        if playerCharacter.lower() in validCharacters:
+            playerOne.character = playerCharacter
+            print("You are now " + playerCharacter)
+
+    #setting up character stats
+    if playerOne is "mario":
+        self.hp = 50
+        self.coins = 50
+    if playerOne is "luigi":
+        self.hp = 55
+        self.coins = 45
+    if playerOne is "kerby":
+        self.hp = 80
+        self.coins = 20
+    if playerOne is "bowser":
+        self.hp = 20
+        self.coins = 80
+
+    #introduction to the game
+    questionWelcome = "Welcome, " + playerName + "." + "You are playing as " + playerCharacter + "."
+    for words in questionWelcome:
+        sys.stdout.write(words)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+    intro1 = "Welcome to Super Mario Code!"
+    for words in intro1:
+        sys.stdout.write(words)
+        sys.stdout.flush()
+        time.sleep(0.01)
+    intro2 = "Make your way through the course to win the game"
+    for words in intro2:
+        sys.stdout.write(words)
+        sys.stdout.flush()
+        time.sleep(0.01)
+    intro3 = "I hope you have fun, but..."
+    for words in intro3:
+        sys.stdout.write(words)
+        sys.stdout.flush()
+        time.sleep(0.01)
+    intro4 = "Do NOT exit the map..... or else."
+    for words in intro4:
+        sys.stdout.write(words)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+
+    os.system("clear")
+    print("The game will start NOW!")
+    game()
+
+
+
+
+
+
+
+
+#call start screen function
