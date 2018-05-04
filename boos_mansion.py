0afe6ac18daeea65d612d60bbd653048022877fb
tnightman-cometh <38928465+tnightman-cometh@users.noreply.github.com>
2018-05-03 21:06:41 -0500

    Update boos_mansion.py

--- boos_mansion.py
+++ boos_mansion.py
@@ -252,8 +252,21 @@ def get_trivia_questions():
     getQuestions = ["What was Mario's original name before he was named Mario?","When was the first Mario game created?",
                 "What kind of extinct animal is Yoshi?","Who is Luigi's doppelganger?", "Who is Mario's doppelganger?",
                 "Who kidnapped the princess in the original Super Mario?", "Who created Mario?", "Who is Princess Peach's best friend?",
-                 "Who is Mario's banana-loving enemy?", "Who is Mario's brother and sidekick?"]
-    getAnswers = ["jumpman", "1981", "dinosaur", "waluigi", "wario", "bowser", "shigeru miyamoto", "princess daisy", "donkey kong", "luigi"]
+                "Who is Mario's banana-loving enemy?", "Who is Mario's brother and sidekick?", "What game did Mario first appear in?",
+                "Who did Mario originally rescue them?", "Are the Mario games the most successful video game series of all time?",
+                "Mario's first 3D platforming game is?", "Which game introduced the Cloud Flower?",
+                "Which console hosted the Super Mario Maker game?"]
+
+
+
+
+"
+", ]
+    getAnswers = ["jumpman", "1981", "dinosaur", "waluigi", "wario", "bowser",
+                  "shigeru miyamoto", "princess daisy", "donkey kong", "luigi", "Donkey Kong",
+                  "Pauline", "Yes", "Super Mario 64", "Super Mario Galaxy", "Nintindo WiiU"]
+
+ ]
 
     for x in range(len(getQuestions)):
         print(getQuestions[x])
